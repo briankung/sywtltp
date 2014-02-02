db8f02677900dd21325be58e67dfba22a5b26239
Brian Kung <brian@callmekung.com>
2014-02-01 23:46:12 -0600

    Adds warning, minor wording changes

--- compile.sh
+++ compile.sh
@@ -2,5 +2,6 @@
 
 clear
 echo "Compiling 'So You Want to Learn to Program' to 'sywtltp.md'"
+echo "Make sure to have two line breaks at the end of each chapter, or it'll look like crap!"
 cat chapters/*.md > sywtltp.md
-echo "Complete"
+echo "Compilation complete!"
